86996ec3c82e57a5045e0991733e4e47f00b9be3
Rashaad Akbar <rushda2000@gmail.com>
2022-04-12 06:23:49 +0530

    feat: Connect to cloud database if the app is running in production

--- apps/backend/database.py
+++ apps/backend/database.py
@@ -1,11 +1,38 @@
-from sqlalchemy import create_engine
+import sqlalchemy
+import pg8000
+import settings
+from google.cloud.sql.connector import Connector
 
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-Base = declarative_base()
+if settings.PRODUCTION:
+    def init_connection_engine() -> sqlalchemy.engine.Engine:
+        def getconn() -> pg8000.dbapi.Connection:
+            connector = Connector()
+            conn: pg8000.dbapi.Connection = connector.connect(
+                settings.POSTGRES_CONNECTION_NAME,
+                "pg8000",
+                user=settings.POSTGRES_USER,
+                password=settings.POSTGRES_PASS,
+                db=settings.POSTGRES_DB,
+            )
+            return conn
+
+        engine = sqlalchemy.create_engine(
+            "postgresql+pg8000://",
+            creator=getconn,
+        )
+        engine.dialect.description_encoding = None
+        return engine
+
+    engine = init_connection_engine()
+    Base = declarative_base()
+
+else:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
+    # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+    engine = sqlalchemy.create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+    Base = declarative_base()
