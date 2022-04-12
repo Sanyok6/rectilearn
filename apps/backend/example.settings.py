7ab567868f249ed32e45665590f69e9a719d9455
VG08 <71305756+VG08@users.noreply.github.com>
2022-04-12 11:03:43 +0530

    fix: add example settings back

---
+++ apps/backend/example.settings.py
@@ -0,0 +1,11 @@
+# Re-name this file to settings.py and supply the value for the variables below
+
+PRODUCTION = False
+
+SECRET_KEY = ""
+ALGORITHM = ""
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+POSTGRES_USER = ""
+POSTGRES_PASS = ""
+POSTGRES_DB = ""
+POSTGRES_CONNECTION_NAME = ""
