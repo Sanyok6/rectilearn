714a0a7615391fa2775fcae2fb9d9dfaa5f63c68
Rashaad Akbar <rushda2000@gmail.com>
2022-04-15 16:04:43 +0530

    update database.py

--- apps/backend/database.py
+++ apps/backend/database.py
@@ -7,35 +7,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 if settings.PRODUCTION:
-    if settings.USE_HEROKU:
-        engine = sqlalchemy.create_engine(
-            "postgresql+psycopg2"+settings.HEROKU_POSTGRESQL_URI
-        )
+    engine = sqlalchemy.create_engine(
+        "postgresql+psycopg2"+settings.POSTGRESQL_URI
+    )
 
-        Base = declarative_base()
-
-    else:
-        def init_connection_engine() -> sqlalchemy.engine.Engine:
-            def getconn() -> pg8000.dbapi.Connection:
-                connector = Connector()
-                conn: pg8000.dbapi.Connection = connector.connect(
-                    settings.POSTGRES_CONNECTION_NAME,
-                    "pg8000",
-                    user=settings.POSTGRES_USER,
-                    password=settings.POSTGRES_PASS,
-                    db=settings.POSTGRES_DB,
-                )
-                return conn
-
-            engine = sqlalchemy.create_engine(
-                "postgresql+pg8000://",
-                creator=getconn,
-            )
-            engine.dialect.description_encoding = None
-            return engine
-
-        engine = init_connection_engine()
-        Base = declarative_base()
+    Base = declarative_base()
 
 else:
     SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
