c60cde4fb0d442a1d0fe7489b88fc356238c90da
Rashaad Akbar <rushda2000@gmail.com>
2022-04-16 12:56:13 +0530

    convert the type of env vars

--- apps/backend/settings.py
+++ apps/backend/settings.py
@@ -1,14 +1,14 @@
+from distutils.util import strtobool
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-PRODUCTION = os.getenv("PRODUCTION")
-USE_RAILWAY = os.getenv("USE_RAILWAY")
+PRODUCTION = strtobool(os.getenv("PRODUCTION"))
+USE_RAILWAY = strtobool(os.getenv("USE_RAILWAY"))
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 # Database
 POSTGRESQL_URI = os.getenv("POSTGRESQL_URI")
-
