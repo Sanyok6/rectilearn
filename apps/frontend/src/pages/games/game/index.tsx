b6ca350c6d3bf27242ce4dba6712b3dec18511e9
NastyPigz <williamzwy06701@gmail.com>
2022-04-16 18:55:24 -0400

    game updates

--- apps/frontend/src/pages/games/game/index.tsx
+++ apps/frontend/src/pages/games/game/index.tsx
@@ -2,8 +2,7 @@ import type { NextPage } from "next";
 import { useState, useEffect, useRef } from "react";
 import Questions from "../../../components/questions";
 
-const mapLayout =
-`
+const mapLayout = `
 |||||||||||||||||
 |               |
 |               |
@@ -18,180 +17,193 @@ const mapLayout =
 mapLayout.shift();
 
 const Game: NextPage = () => {
-    const cRef = useRef<HTMLCanvasElement>(null);
-    const [reload, setReload] = useState<boolean>(false);
-    useEffect(() => {
-        window.onresize = () => {
-            setReload(reload => !reload)
-        }
-        if (cRef.current) {
-            cRef.current.style.width = "100vw";
-            cRef.current.style.height= "100vh";
-            cRef.current.style.position = "fixed";
-            cRef.current.focus();
-        }
-        async function Launch() {
-            const kab = await import("kaboom").then((mod) => mod.default({ canvas: cRef.current || undefined, background: [137, 142, 140], width: cRef.current?.scrollWidth, height: cRef.current?.scrollHeight }));
-            await loadSprite("bean", "/sprites/bean.png");
-            await loadSprite("wall", "/sprites/wall.jpeg");
-            await loadSprite("water", "/sprites/water.jpeg");
-            await loadSprite("grass", "/sprites/grass.png");
-            await loadSprite("bird", "/sprites/bird.png");
-            await loadSprite("bg", "/sprites/grassBg.png");
-            await loadSprite("rod", "/sprites/rod.png");
-            let start = false;
-            const SPEED = 320;
-            add([
-                sprite("bg", {
-                    width: width(),
-                    height: height()
-                }),
-                pos(0, 0),
-                fixed()
-            ]);
-            const beforeStart = add([
-                text("Click Anywhere To Start", {
-                    size: width() / 25
-                }),
-                pos(center()),
-                kab.origin("center"),
-            ]);
-            async function startGame() {
-                if (start) return;
-                start = true;
-                const itemXLen = mapLayout[0].length;
-                const itemYLen = mapLayout.length;
-                const wallXY = Math.max(width() / itemXLen, height() / itemYLen)
-                const mapObj = addLevel(mapLayout, {
-                    width: wallXY,
-                    height: wallXY,
-                    "|": () => [
-                        sprite("wall", {
-                            width: wallXY,
-                            height: wallXY
-                        }),
-                        area(),
-                        solid()
-                    ],
-                    "$": () => [
-                        sprite("water", {
-                            width: wallXY,
-                            height: wallXY
-                        }),
-                        area(),
-                        solid(),
-                        "water"
-                    ],
-                    " ": () => [
-                        sprite("grass", {
-                            width: wallXY,
-                            height: wallXY
-                        })
-                    ]
-                });
-                const player = add([
-                    sprite("bean", {
-                        width: wallXY,
-                    }),
-                    pos(mapObj.getPos(itemXLen / 2, itemYLen / 2)),
-                    area(),
-                    solid()
-                ]);
-                const rod = add([
-                    sprite("rod", {
-                        height: wallXY
-                    }),
-                    pos(),
-                    follow(player, vec2(wallXY * 0.7, -wallXY * 0.3)),
-                    rotate(0),
-                    spin()
-                ]);
-                const counter = add([
-                    text("Cash: 0"),
-                    pos(0, 0),
-                    fixed(),
-                    { value: 0 },
-                ])
-                function spin() {
-                    let spinning = false
-                    return {
-                        id: "spin",
-                        update() {
-                            const t = this as any;
-                            if (spinning) {
-                                t.angle += 1200 * dt()
-                                if (t.angle >= 360) {
-                                    t.angle = 0
-                                    spinning = false
-                                }
-                            }
-                        },
-                        spin() {
-                            spinning = true
-                        },
-                    }
-                }
-                beforeStart.destroy();
-                player.onUpdate(() => {
-                    camPos(player.pos);
-                });
-                onKeyDown("left", () => {
-                    player.flipX(true);
-                    rod.flipX(true);
-                    rod.follow.offset = vec2(-wallXY * 0.7, -wallXY * 0.3);
-                    player.move(-SPEED, 0);
-                });
-                onKeyDown("right", () => {
-                    player.flipX(false);
-                    rod.flipX(false);
-                    rod.follow.offset = vec2(wallXY * 0.7, -wallXY * 0.3)
-                    player.move(SPEED, 0);
-                });
-                onKeyDown("up", () => {
-                    player.move(0, -SPEED);
-                });
-                onKeyDown("down", () => {
-                    player.move(0, SPEED);
-                });
-                onKeyPress("space", () => {
-                    let touching = false;
-                    every("water", (s) => {
-                        if (player.isTouching(s)) {
-                            touching = true;
-                        }
-                    });
-                    if (touching) {
-                        const bird = add([
-                            sprite("bird", {
-                                width: wallXY
-                            }),
-                            pos(player.pos)
-                        ]);
-                        bird.onUpdate(() => {
-                            bird.flipX(Math.random() < 0.5);
-                            bird.moveTo(bird.pos.x - rand(-20, 20), bird.pos.y - rand(-20, 20))
-                        })
-                        wait(2, () => {
-                            destroy(bird);
-                        });
-                        rod.spin();
-                        counter.value ++;
-                        counter.text = "Cash: " + counter.value
-                    }
-                });
-            }
-            onTouchEnd(startGame);
-            onClick(startGame);
-        }
-        Launch();
-        return () => every(destroy);
-    }, [reload]);
-    return (
-        <>
-            <canvas ref={cRef}></canvas>
-            {/* <Questions question="t" answer="t" /> */}
-        </>
-    )
-}
+	const cRef = useRef<HTMLCanvasElement>(null);
+	const [reload, setReload] = useState<boolean>(false);
+	useEffect(() => {
+		window.onresize = () => {
+			setReload((reload) => !reload);
+		};
+		if (cRef.current) {
+			cRef.current.style.width = "100vw";
+			cRef.current.style.height = "100vh";
+			cRef.current.style.position = "fixed";
+			cRef.current.focus();
+		}
+		async function Launch() {
+			const kab = await import("kaboom").then((mod) =>
+				mod.default({
+					canvas: cRef.current || undefined,
+					background: [137, 142, 140],
+					width: cRef.current?.scrollWidth,
+					height: cRef.current?.scrollHeight,
+				})
+			);
+			await loadSprite("bean", "/sprites/bean.png");
+			await loadSprite("wall", "/sprites/wall.jpeg");
+			await loadSprite("water", "/sprites/water.jpeg");
+			await loadSprite("grass", "/sprites/grass.png");
+			await loadSprite("bird", "/sprites/bird.png");
+			await loadSprite("bg", "/sprites/grassBg.png");
+			await loadSprite("rod", "/sprites/rod.png");
+			let start = false;
+			const SPEED = 320;
+			add([
+				sprite("bg", {
+					width: width(),
+					height: height(),
+				}),
+				pos(0, 0),
+				fixed(),
+			]);
+			const beforeStart = add([
+				text("Click Anywhere To Start", {
+					size: width() / 25,
+				}),
+				pos(center()),
+				kab.origin("center"),
+			]);
+			async function startGame() {
+				if (start) return;
+				start = true;
+				const itemXLen = mapLayout[0].length;
+				const itemYLen = mapLayout.length;
+				const wallXY = Math.max(
+					width() / itemXLen,
+					height() / itemYLen
+				);
+				const mapObj = addLevel(mapLayout, {
+					width: wallXY,
+					height: wallXY,
+					"|": () => [
+						sprite("wall", {
+							width: wallXY,
+							height: wallXY,
+						}),
+						area(),
+						solid(),
+					],
+					$: () => [
+						sprite("water", {
+							width: wallXY,
+							height: wallXY,
+						}),
+						area(),
+						solid(),
+						"water",
+					],
+					" ": () => [
+						sprite("grass", {
+							width: wallXY,
+							height: wallXY,
+						}),
+					],
+				});
+				const player = add([
+					sprite("bean", {
+						width: wallXY,
+					}),
+					pos(mapObj.getPos(itemXLen / 2, itemYLen / 2)),
+					area(),
+					solid(),
+				]);
+				const rod = add([
+					sprite("rod", {
+						height: wallXY,
+					}),
+					pos(),
+					follow(player, vec2(wallXY * 0.7, -wallXY * 0.3)),
+					rotate(0),
+					spin(),
+				]);
+				const counter = add([
+					text("Cash: 0"),
+					pos(0, 0),
+					fixed(),
+					{ value: 0 },
+				]);
+				function spin() {
+					let spinning = false;
+					return {
+						id: "spin",
+						update() {
+							const t = this as any;
+							if (spinning) {
+								t.angle += 1200 * dt();
+								if (t.angle >= 360) {
+									t.angle = 0;
+									spinning = false;
+								}
+							}
+						},
+						spin() {
+							spinning = true;
+						},
+					};
+				}
+				beforeStart.destroy();
+				player.onUpdate(() => {
+					camPos(player.pos);
+				});
+				onKeyDown("left", () => {
+					player.flipX(true);
+					rod.flipX(true);
+					rod.follow.offset = vec2(-wallXY * 0.7, -wallXY * 0.3);
+					player.move(-SPEED, 0);
+				});
+				onKeyDown("right", () => {
+					player.flipX(false);
+					rod.flipX(false);
+					rod.follow.offset = vec2(wallXY * 0.7, -wallXY * 0.3);
+					player.move(SPEED, 0);
+				});
+				onKeyDown("up", () => {
+					player.move(0, -SPEED);
+				});
+				onKeyDown("down", () => {
+					player.move(0, SPEED);
+				});
+				onKeyPress("space", () => {
+					let touching = false;
+					every("water", (s) => {
+						if (player.isTouching(s)) {
+							touching = true;
+						}
+					});
+					if (touching) {
+						const bird = add([
+							sprite("bird", {
+								width: wallXY,
+							}),
+							pos(player.pos),
+						]);
+						bird.onUpdate(() => {
+							bird.flipX(Math.random() < 0.5);
+							bird.moveTo(
+								bird.pos.x - rand(-20, 20),
+								bird.pos.y - rand(-20, 20)
+							);
+						});
+						wait(2, () => {
+							destroy(bird);
+						});
+						rod.spin();
+						counter.value++;
+						counter.text = "Cash: " + counter.value;
+					}
+				});
+			}
+			onTouchEnd(startGame);
+			onClick(startGame);
+		}
+		Launch();
+		return () => every(destroy);
+	}, [reload]);
+	return (
+		<>
+			<canvas ref={cRef}></canvas>
+			{/* <Questions question="t" answer="t" /> */}
+		</>
+	);
+};
 
 export default Game;
--- apps/frontend/src/pages/games/game/lavaGame.tsx
+++ apps/frontend/src/pages/games/game/lavaGame.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef } from "react";
 import Questions from "../../../components/questions";
-
 
 /*
 & = player
@@ -22,194 +21,239 @@ const gameMap = `
 |                  |
 |                  |
 |                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
 |$$$$$$$$$$$$$$$$$$|
 |$$$$$$$$$$$$$$$$$$|
 |$$$$$$$$$$$$$$$$$$|
 |$$$$$$$$$$$$$$$$$$|
-|$$$$$$$$$$$$$$$$$$|
-|$$$$$$$$$$$$$$$$$$|`.split('\n');
+|$$$$$$$$$$$$$$$$$$|`.split("\n");
 gameMap.shift();
 
-
 const lavaGame = () => {
-    const cRef = useRef<HTMLCanvasElement>(null);
-    const [reload, setReload] = useState<boolean>(false);
-    const [open, setOpen] = useState<boolean>(true);
-    useEffect(() => {
-        window.onresize = () => {
-            setReload(reload => !reload)
-        }
+	const cRef = useRef<HTMLCanvasElement>(null);
+	const [reload, setReload] = useState<boolean>(false);
+	const [open, setOpen] = useState<boolean>(true);
+	useEffect(() => {
+		window.onresize = () => {
+			setReload((reload) => !reload);
+		};
 
-        if (cRef.current) {
-            cRef.current.style.width = "100vw";
-            cRef.current.style.height= "100vh";
-            cRef.current.style.position = "fixed";
-            cRef.current.focus();
-        }
-        async function Launch() {
-            const kab = await import("kaboom").then((mod) => mod.default({ debug: true, canvas: cRef.current || undefined, background: [137, 142, 140], width: cRef.current?.scrollWidth, height: cRef.current?.scrollHeight }));
-            const SPEED = 600;
-            const lavaRaiseSpeed = 10;
-            const JUMP_FORCE = 800;
-            const NUM_PLATFORMS = 20;
-            await loadBean();
-            await loadSprite("grayBg", "/sprites/grayBg.webp");
-            await loadSprite("lava", "/sprites/lava.jpeg");
-            await loadSprite("wall", "/sprites/wall.jpeg");
-            await loadSprite("platform", "/sprites/platform.jpeg");
-            gravity(2000);
-
-            add([
-                sprite("grayBg", {
-                    width: width(),
-                    height: height()
-                }),
-                pos(0, 0),
-                fixed(),
-            ]);
-
-            const mapX = gameMap[0].length;
-            const mapY = gameMap.length;
-
-            const wallXY = Math.max(width() / mapX, height() / mapY);
-
-            add([
-                text("Points: 0"),
-                pos(0, 0),
-                fixed(),
-                { value: 0 },
-            ]);
-
-            scene("gameover", () => {
-                add([
-                    text("Game Over!", {
-                        size: width() / 25
-                    }),
-                    pos(center()),
-                    kab.origin("center"),
-                ]);
-            });
-
-            for (let i = 1; i < NUM_PLATFORMS; i++) {
-                add([
-                    sprite("wall", {
-                        width: wallXY,
-                        height: wallXY / 2
-                    }),
-                    area(),
-                    pos(rand(0 + wallXY * 2, width() - wallXY * 2), -i * (wallXY * 60) / NUM_PLATFORMS + wallXY * 24),
-                    solid(),
-                    kab.origin("center"),
-                    "platform",
-                    {
-                        speed: rand(50, 120),
-                        dir: choose([-1, 1]),
-                    },
-                ])
+		if (cRef.current) {
+			cRef.current.style.width = "100vw";
+			cRef.current.style.height = "100vh";
+			cRef.current.style.position = "fixed";
+			cRef.current.focus();
+			cRef.current.click();
+		}
+		setInterval(() => {
+			if (cRef.current) {
+                cRef.current.click();
+			    cRef.current.focus();
             }
+		}, 1);
+		async function Launch() {
+			const kab = await import("kaboom").then((mod) =>
+				mod.default({
+					debug: true,
+					canvas: cRef.current || undefined,
+					background: [137, 142, 140],
+					width: cRef.current?.scrollWidth,
+					height: cRef.current?.scrollHeight,
+				})
+			);
+			await loadBean();
+			await loadSprite("grayBg", "/sprites/grayBg.webp");
+			await loadSprite("lava", "/sprites/lava.jpeg");
+			await loadSprite("wall", "/sprites/wall.jpeg");
+			await loadSprite("platform", "/sprites/platform.jpeg");
+			gravity(2000);
 
-            const player = add([
-                sprite("bean", {
-                    width: wallXY
-                }),
-                pos(center()),
-                kab.origin("center"),
-                area(),
-                body({ jumpForce: JUMP_FORCE, }),
-            ]);
+			add([
+				sprite("grayBg", {
+					width: width(),
+					height: height(),
+				}),
+				pos(0, 0),
+				fixed(),
+			]);
 
-            player.pos = get("platform")[0].pos.sub(0, 64)
+			const mapX = gameMap[0].length;
+			const mapY = gameMap.length;
 
-            addLevel(gameMap, {
-                width: wallXY,
-                height: wallXY,
-                "|": () => [
-                    sprite("wall", {
-                        width: wallXY,
-                        height: wallXY
-                    }),
-                    area(),
-                    solid(),
-                    "wall"
-                ],
-                "$": () => [
-                    sprite("lava", {
-                        width: wallXY,
-                        height: wallXY
-                    }),
-                    area(),
-                    "lava"
-                ],
-                "_": () => [
-                    sprite("platform", {
-                        width: wallXY,
-                        height: wallXY
-                    }),
-                    area(),
-                    solid(),
-                    "platform",
-                    outview({ hide: true, pause: true }),
-                    { answered: false }
-                ],
-            });
+			const wallXY = Math.max(width() / mapX, height() / mapY);
 
-            onUpdate("platform", (p) => {
-                if (player.isTouching(p)) {
-                    p.move(0, lavaRaiseSpeed);
-                    return;
-                }
-                p.move(0, lavaRaiseSpeed);
-                p.move(p.dir * p.speed, 0);
-                every("lava", (o) => {
-                    if (p.isTouching(o)) {
-                        destroy(p);
-                    }
-                });
-                if (p.pos.x < 0 + wallXY * 2 || p.pos.x > width() - wallXY * 2) {
-                    p.dir = -p.dir
-                }
-            });
-            player.onCollide("lava", _ => go("gameover"))
-            player.onUpdate(() => {
-                // if (player.pos.y > get("lava")[0].pos.y) {
-                //     go("gameover");
-                // }
-                every("wall", (o) => {
-                    if (player.isTouching(o)) {
-                        player.moveTo(o.pos.x < player.pos.x ? player.pos.x + 1 : player.pos.x - 1, player.pos.y);
-                    }
-                });
-                camPos(player.pos);
-            });
-            onKeyPress("space", () => {
-                player.doubleJump();
-            });
-            onKeyDown("left", () => {
-                player.flipX(true);
-                player.move(-SPEED, 0);
-            });
-            onKeyDown("right", () => {
-                player.flipX(false);
-                player.move(SPEED, 0);
-            })
-        }
-        Launch();
-        return () => every(destroy);
-    }, [reload]);
-    return (
-        <>
-            <canvas ref={cRef}></canvas>
-            <Questions question="What is 2+2" answer="4" open={open} isOpen={setOpen} />
-        </>
-    )}
+            const SPEED = wallXY * 10;
+			const lavaRaiseSpeed = 10;
+			const JUMP_FORCE = wallXY * 16;
+			const NUM_PLATFORMS = 20;
+
+			add([text("Points: 0"), pos(0, 0), fixed(), { value: 0 }]);
+
+			scene("gameover", () => {
+				add([
+					text("Game Over!", {
+						size: width() / 25,
+					}),
+					pos(center()),
+					kab.origin("center"),
+				]);
+			});
+
+			for (let i = 1; i < NUM_PLATFORMS; i++) {
+				add([
+					sprite("wall", {
+						width: wallXY,
+						height: wallXY / 2,
+					}),
+					area(),
+					pos(
+						rand(0 + wallXY * 2, width() - wallXY * 2),
+						(-i * (wallXY * 60)) / NUM_PLATFORMS + wallXY * 12
+					),
+					solid(),
+					kab.origin("center"),
+					"platform",
+					{
+						speed: rand(50, 120),
+						dir: choose([-1, 1]),
+                        answered: false
+					},
+                    outview({
+                        hide: true
+                    })
+				]);
+			}
+
+			function spin(speed = 1200) {
+				let spinning = false;
+				return {
+					require: ["rotate"],
+					update(this: any) {
+						if (!spinning) {
+							return;
+						}
+						this.angle -= speed * dt();
+						if (this.angle <= -360) {
+							spinning = false;
+							this.angle = 0;
+						}
+					},
+					spin() {
+						spinning = true;
+					},
+				};
+			}
+
+			const player = add([
+				sprite("bean", {
+					width: wallXY,
+				}),
+				pos(center()),
+				kab.origin("center"),
+				area(),
+				body({ jumpForce: JUMP_FORCE }),
+				spin(),
+				rotate(0),
+			]);
+
+			player.pos = get("platform")[0].pos.sub(0, 64);
+
+			addLevel(gameMap, {
+				width: wallXY,
+				height: wallXY,
+				"|": () => [
+					sprite("wall", {
+						width: wallXY,
+						height: wallXY,
+					}),
+					area(),
+					solid(),
+					"wall",
+				],
+				$: () => [
+					sprite("lava", {
+						width: wallXY,
+						height: wallXY,
+					}),
+					area(),
+					"lava",
+				],
+				// _: () => [
+				// 	sprite("platform", {
+				// 		width: wallXY,
+				// 		height: wallXY,
+				// 	}),
+				// 	area(),
+				// 	solid(),
+				// 	"platform",
+				// 	outview({ hide: true, pause: true }),
+				// 	{ answered: false },
+				// ],
+			});
+
+			onUpdate("platform", (p) => {
+                console.log(p);
+                if (p.isOutOfView()) return;
+				if (player.isTouching(p)) {
+					player.move(p.dir * p.speed, 0);
+				}
+				p.move(0, lavaRaiseSpeed);
+				p.move(p.dir * p.speed, 0);
+				every("lava", (o) => {
+					if (p.isTouching(o)) {
+						destroy(p);
+					}
+				});
+				if (
+					p.pos.x < 0 + wallXY * 2 ||
+					p.pos.x > width() - wallXY * 2
+				) {
+					p.dir = -p.dir;
+				}
+			});
+			player.onCollide("lava", (_) => go("gameover"));
+			player.onCollide("wall", (o) => {
+				if (player.isTouching(o)) {
+					player.moveTo(
+						o.pos.x < player.pos.x
+							? player.pos.x + 1
+							: player.pos.x - 1,
+						player.pos.y
+					);
+				}
+			});
+			player.onUpdate(() => {
+                if (player.pos.y > 1000) go("gameover");
+				camPos(player.pos);
+			});
+			player.onDoubleJump(() => {
+				player.spin();
+			});
+			onKeyPress("space", () => {
+				player.doubleJump();
+			});
+			onKeyDown("left", () => {
+				player.flipX(true);
+				player.move(-SPEED, 0);
+			});
+			onKeyDown("right", () => {
+				player.flipX(false);
+				player.move(SPEED, 0);
+			});
+		}
+		Launch();
+		return () => every(destroy);
+	}, [reload]);
+	return (
+		<>
+			<canvas ref={cRef}></canvas>
+			<Questions
+				question="What is 2+2"
+				answer="4"
+				open={open}
+				isOpen={setOpen}
+			/>
+		</>
+	);
+};
 
 export default lavaGame;
