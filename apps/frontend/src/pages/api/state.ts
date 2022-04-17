f7fa853836f458fcc584f4bad4ce88c911f483b1
Rashaad Akbar <rushda2000@gmail.com>
2022-04-17 16:39:55 +0530

    fix bug

--- apps/frontend/src/pages/api/state.ts
+++ apps/frontend/src/pages/api/state.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.cookies.access_token) {
+    if (req.cookies.Authorization) {
         res.status(200).json(true);
     } else {
         res.status(200).json(false);
