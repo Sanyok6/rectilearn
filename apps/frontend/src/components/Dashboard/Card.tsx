bdee097f4a4ef5ad34e47788683f4d69494025af
Rashaad Akbar <rushda2000@gmail.com>
2022-04-18 11:10:48 +0530

    add StudySetEditModal

--- apps/frontend/src/components/Dashboard/Card.tsx
+++ apps/frontend/src/components/Dashboard/Card.tsx
@@ -9,8 +9,26 @@ import {
     useBreakpointValue,
     useColorModeValue,
     Tooltip,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalHeader,
+    FormControl,
+    FormLabel,  
+    ModalOverlay,
+    ModalFooter,
+    useDisclosure,
+    ModalCloseButton,
+    Input,
+    Menu,
+    MenuButton,
+    MenuList,
+    InputGroup,
+    InputRightElement,
+    useToast
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { CloseIcon } from "@chakra-ui/icons";
 import { Image } from '../../utils/next-chakra-image';
 
 import { Icon, IconButton, IconButtonProps, LightMode } from '@chakra-ui/react';
@@ -64,23 +82,24 @@ export interface StudySet {
 
 interface Props {
     studySet: StudySet;
-    rootProps?: StackProps
+    rootProps?: StackProps;
+    updateStudySet: Function;
 }
 
 const Card = (props: Props) => {
     const imageUrl = "https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc="
-    const { studySet, rootProps } = props;
+    const { studySet, updateStudySet, rootProps } = props;
     const { id, subject } = studySet;
     const [isLoading, setLoading] = useState<boolean>(true);
-    
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     const deleteStudySet = async (e: any) => {
         // e.preventDefault();
         // console.log("stopped ", e)
-        // const resp = await fetch(`/api/studysets/${id}/delete_study_set/`, {
-        //     method: "DELETE"
-        // });
+        const resp = await fetch(`/api/studysets/${id}/delete_study_set/`, {
+            method: "DELETE"
+        });
         // console.log(resp);
-        await fetch("/");
     }
 
     return (
@@ -150,9 +169,11 @@ const Card = (props: Props) => {
                         isFullWidth
                         flex={1}
                         fontSize={'sm'}
+                        onClick={onOpen}
                     >
                         Edit
                     </Button>
+                    <EditStudySetModal isOpen={isOpen} onClose={onClose} currentStudySet={studySet} updateStudySet={updateStudySet} />
                     <Button
                         colorScheme="red"
                         isFullWidth
@@ -167,5 +188,130 @@ const Card = (props: Props) => {
         </Stack>
     )
 }
+
+
+const EditStudySetModal = (props: any) => {
+    const { currentStudySet, updateStudySet } = props;
+    const { id, subject: currentSubject, questions: currentQuestions } = currentStudySet;
+
+    const { isOpen, onClose: oC } = props;
+    const [v, setV] = useState<string>(currentSubject);
+    const [questions, setQuestions] = useState<Array<StudySetQuestion>>(currentQuestions);
+    const toast = useToast();
+
+    function onClose() {
+        setQuestions(currentQuestions);
+        setV(currentSubject);
+        oC();
+    }
+
+    const handleSubmission = async () => {
+        const values = {
+            subject: v,
+            questions: questions
+        };
+
+        const res = await fetch(`/api/studysets/${id}/update/`, {
+            method: "PUT",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(values)
+        })
+
+        if (!res.ok){
+            toast({
+                title: "Something went wrong",
+                description: res ? (await res.json()).detail || "err" : "Something went wrong while creating studyset. Please try again.",
+                status: "error",
+                isClosable: true,
+                duration: 4000
+            })
+            return;
+        }
+        
+        updateStudySet(await res.json());
+
+        onClose();
+    };
+
+	return (
+        <>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Create study set</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <FormControl>
+                            <FormLabel htmlFor="subject">Subject or Name</FormLabel>
+                            <Input id="subject" placeholder="Your subject/name here" value={v} onChange={(e) => setV(e.target.value)} />
+                        </FormControl>
+                        {
+                            questions.map((i, ind) => (
+                                <FormControl key={ind}>
+                                    <FormLabel htmlFor={`q${ind}`}>Question</FormLabel>
+                                    <Input id={`q${ind}`} placeholder="Your question here" value={i.question} onChange={(e) => setQuestions(qs => {
+                                        let newQ = [...qs];
+                                        newQ[ind] = {
+                                            ...newQ[ind],
+                                            question: e.target.value
+                                        }
+                                        return newQ;
+                                    })} />
+                                    <Menu>
+                                        <MenuButton as={Button} mt={3}>
+                                            View Answers
+                                        </MenuButton>
+                                        <MenuList maxWidth={"20vw"}>
+                                            {i.answers.map((ite, idx) => (
+                                                <InputGroup key={idx} display="flex" flexDir="row" alignItems="center" justifyContent="center">
+                                                    <Input alignSelf="center" mx={3} value={ite} onChange={(e) => setQuestions(qs => {
+                                                        let newQ = [...qs];
+                                                        if (newQ[ind].answers[idx] !== undefined) {
+                                                          newQ[ind] = {
+                                                            ...newQ[ind],
+                                                            answers: newQ[ind].answers.map((answer, i) => (i === idx ? e.target.value : answer)),
+                                                          };
+                                                        }
+                                                        return newQ;
+                                                    })} my={3} />
+                                                    <InputRightElement onClick={() => setQuestions(qs => {
+                                                        let newQ = [...qs];
+                                                        newQ[ind] = {
+                                                            ...newQ[ind],
+                                                            answers: newQ[ind].answers.filter((_, i) => i !== idx)
+                                                        }
+                                                        return newQ;
+                                                    })}><CloseIcon /></InputRightElement>
+                                                </InputGroup>
+                                            ))}
+                                            <Button mt={3} onClick={() => setQuestions(qs => {
+                                                let newQ = [...qs];
+                                                newQ[ind] = {
+                                                    ...newQ[ind],
+                                                    answers: [...newQ[ind].answers, ""]
+                                                }
+                                                return newQ;
+                                            })}>Add Answer</Button>
+                                        </MenuList>
+                                    </Menu>
+                                </FormControl>
+                            ))
+                        }
+                        <Button mt={5} width={"80%"} colorScheme="blue" onClick={() => setQuestions(questions => [...questions, { question: "", answers: [] }])} >Add Question</Button>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3} onClick={handleSubmission}>
+                            Edit
+                        </Button>
+                        <Button colorScheme="red" mr={3} onClick={onClose}>
+                            Cancel
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+	);
+}
+
 
 export default Card;
