5bcc24609d5f601ebd6dd17c35940fe2207dc272
Sanyok6 <tishchenkoa2008@gmail.com>
2022-05-14 19:50:58 -0400

    fixed card grid

--- apps/frontend/src/components/Dashboard/CardGrid.tsx
+++ apps/frontend/src/components/Dashboard/CardGrid.tsx
@@ -5,10 +5,10 @@ const CardGrid = (props: SimpleGridProps) => {
   const columns = React.useMemo(() => {
     const count = React.Children.toArray(props.children).filter(React.isValidElement).length
     return {
-      base: Math.min(2, count),
-      md: Math.min(3, count),
-      lg: Math.min(4, count),
-      xl: Math.min(5, count),
+      base: Math.min(1, count),
+      md: Math.min(2, count),
+      lg: Math.min(3, count),
+      xl: Math.min(4, count),
     }
   }, [props.children])
 
