17219cba91fe1728733fde51b693f51f33201610
VG <71305756+VG08@users.noreply.github.com>
2022-07-10 17:47:56 +0530

    readd header auth alongside cookie

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -31,22 +31,23 @@ class OAuth2PasswordBearerCookie(OAuth2):
         super().__init__(flows=flows, scheme_name=scheme_name, auto_error=auto_error)
 
     async def __call__(self, request: Request) -> Optional[str]:
-        # header_authorization: str = request.headers.get("Authorization")
+        header_authorization: str = request.headers.get("Authorization")
         cookie_authorization: str = request.cookies.get("Authorization")
 
-        # header_scheme, header_param = get_authorization_scheme_param(
-        #     header_authorization
-        # )
+        header_scheme, header_param = get_authorization_scheme_param(
+            header_authorization
+        )
         cookie_scheme, cookie_param = get_authorization_scheme_param(
             cookie_authorization
         )
+        print(header_authorization, header_scheme, header_param)
+        if header_scheme.lower() == "bearer":
+            print("here")
+            authorization = True
+            scheme = header_scheme
+            param = header_param
 
-        # if header_scheme.lower() == "bearer":
-        #     authorization = True
-        #     scheme = header_scheme
-        #     param = header_param
-
-        if cookie_scheme.lower() == "bearer":
+        elif cookie_scheme.lower() == "bearer":
             authorization = True
             scheme = cookie_scheme
             param = cookie_param
