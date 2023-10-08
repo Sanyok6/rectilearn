87278c5f6c7bf987579f5d505890bc3139375d9b
Sanyok6 <>
2023-10-08 09:34:17 -0400

    Fix backend error

--- apps/frontend-next/next.config.js
+++ apps/frontend-next/next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
 		return [
 			{
 				source: "/api/:path*/",
-				destination: `http://localhost:8000/:path*/`,
+				destination: `http://0.0.0.0:8000/:path*/`,
 			},
 			// {
 			//   source: "/api/:path*/",
